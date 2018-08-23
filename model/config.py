84b9d6eb18d2670b4093562707dca03fa2a4d145
Samuel Jackson <samueljackson@outlook.com>
2018-08-23 13:57:40 +0100

    Fix config path for packaging

--- model/config.py
+++ model/config.py
@@ -12,7 +12,7 @@ class ModelConfig(HasTraits):
 
     use_multiprocessing = Bool(False, desc='to use multiprocessing when calculating coverage.')
     reflection_divergence_degrees = Float(0.3, desc='the default half-width of the divergence of scattered beams, in degrees.')
-    default_detector_filename = String("../instruments/TOPAZ_geom_all_2011.csv", desc="the default file and path to the detectors .CSV geometry specification file.")
+    default_detector_filename = String("instruments/TOPAZ_geom_all_2011.csv", desc="the default file and path to the detectors .CSV geometry specification file.")
     force_pure_python = Bool(False, desc='to force calculations to use pure Python, rather than inline C code.')
 
 #Initialize the configuration file
@@ -20,6 +20,7 @@ cfg = ModelConfig()
 
 #Fix filenames' relative paths to point to package data properly.
 import os
-cfg.default_detector_filename = os.path.join(os.path.dirname(__file__), cfg.default_detector_filename)
+
+cfg.default_detector_filename = os.path.join(os.path.dirname(os.path.dirname(__file__)), cfg.default_detector_filename)
 
 
